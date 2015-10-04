449df6b54c4f263894d4eb716d3d79be0ce655b2
doraTeX <taylorkgb@gmail.com>
2015-10-05 03:57:34 +0900

    Modified NFD の実装を，独自実装から CFStringGetFileSystemRepresentation に切り替え

--- Sources/NSString-Normalization.m
+++ Sources/NSString-Normalization.m
@@ -36,7 +36,13 @@
 
 - (NSString*)normalizedStringWithModifiedNFD
 {
-    return [self normalizedStringConsideringCompositionExclusionsWithBaseNormalizationSelector:@selector(decomposedStringWithCanonicalMapping)];
+    CFStringRef sourceStr = (__bridge CFStringRef)self;
+    size_t length = strlen(self.UTF8String) * FACTOR;
+    char *destStr = (char*)malloc(sizeof(char) * length);
+    Boolean success = CFStringGetFileSystemRepresentation(sourceStr, destStr, length);
+    NSString *result = success ? [NSString stringWithUTF8String:destStr] : self;
+    free(destStr);
+    return result;
 }
 
 
