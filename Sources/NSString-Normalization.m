044ae17cadb9f3a09626d2204a449ed3a41b50cf
doraTeX <taylorkgb@gmail.com>
2015-10-07 00:43:55 +0900

    Ver. 1.10.5

--- Sources/NSString-Normalization.m
+++ Sources/NSString-Normalization.m
@@ -1,13 +1,11 @@
 #import "NSString-Normalization.h"
 
-#define FACTOR 256
-
 @implementation NSString (Normalization)
 - (NSString*)normalizedStringWithModifiedNFD
 {
     CFStringRef sourceStr = (__bridge CFStringRef)self;
-    size_t length = strlen(self.UTF8String) * FACTOR;
-    char *destStr = (char*)malloc(sizeof(char) * length);
+    CFIndex length = CFStringGetMaximumSizeOfFileSystemRepresentation(sourceStr);
+    char *destStr = (char*)malloc(length);
     Boolean success = CFStringGetFileSystemRepresentation(sourceStr, destStr, length);
     NSString *result = success ? [NSString stringWithUTF8String:destStr] : self;
     free(destStr);
