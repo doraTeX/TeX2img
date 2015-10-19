421a5a1fc9cab8d172c62a2a2d6ad6196eb44953
doraTeX <taylorkgb@gmail.com>
2015-10-19 23:41:41 +0900

    Ver. 2.0.6

--- Sources/mainc.m
+++ Sources/mainc.m
@@ -10,7 +10,7 @@
 #import "NSDictionary-Extension.h"
 
 #define OPTION_NUM 51
-#define VERSION "2.0.6b2"
+#define VERSION "2.0.6"
 #define DEFAULT_MAXIMAL_NUMBER_OF_COMPILATION 3
 
 #define ENABLED "enabled"
