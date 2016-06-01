b08f375c7182ab7f8e55b2cffe6ff7f4162dc8a5
doraTeX <taylorkgb@gmail.com>
2016-06-01 16:53:46 +0900

    BB取得時にgsがエラー終了しても，BBらしき文字列を吐いていれば成功と見なすように (#74)

--- Sources/Converter.m
+++ Sources/Converter.m
@@ -400,12 +400,6 @@
                                                  ]
                                          quiet:quietFlag];
         
-        if (!success) {
-            [controller showExecError:@"Ghostscript"];
-            [fileManager removeItemAtPath:bboxFilePath error:nil];
-            return nil;
-        }
-        
         NSString *bboxOutput = [NSString stringWithContentsOfFile:bboxFilePath encoding:NSUTF8StringEncoding error:NULL];
         [fileManager removeItemAtPath:bboxFilePath error:nil];
         
@@ -415,6 +409,7 @@
         for (NSString *line in [bboxOutput componentsSeparatedByString:@"\n"]) {
             if ((line.length >= 5) && [[line substringWithRange:NSMakeRange(0, 5)] isEqualToString:@"Page "]) { // "Page "から始まる行について
                 currentPage = [line substringFromIndex:5].integerValue;
+                success = YES;
                 continue;
             }
             if ((line.length >= 14) && [[line substringWithRange:NSMakeRange(0, 14)] isEqualToString:@"%%BoundingBox:"]) { // "%%BoundingBox:"から始まる行について
@@ -426,6 +421,12 @@
                 continue;
             }
         }
+
+        if (!success) {
+            [controller showExecError:@"Ghostscript"];
+            [fileManager removeItemAtPath:bboxFilePath error:nil];
+            return nil;
+        }
     }
     
     return bboxDictionary[key];
@@ -434,7 +435,13 @@
 - (BOOL)isEmptyPage:(NSString*)pdfPath
                page:(NSUInteger)page
 {
-    return [[self bboxStringOfPdf:pdfPath page:page hires:NO] isEqualToString:EMPTY_BBOX];
+    NSString *bbStr = [self bboxStringOfPdf:pdfPath page:page hires:NO];
+    
+    if (!bbStr) {
+        return NO;
+    }
+    
+    return [bbStr isEqualToString:EMPTY_BBOX];
 }
 
 - (BOOL)willEmptyPageBeCreated:(NSString*)pdfPath
@@ -457,6 +464,11 @@
                                                                   hires:NO
                                                               addHeader:YES] :
         [self bboxStringOfPdf:pdfPath page:page hires:NO];
+    
+    if (!bbStr) {
+        return nil;
+    }
+    
     // ここで HiResBoundingBox を使うと，速度優先でビットマップ画像を生成する際に，小数点以下が切り捨てられて端が欠けてしまうことがある。よって，大きめに見積もる非HiResのBBoxを使うのが得策。
     
     return [NSString stringWithFormat:@"{\\catcode37=13 \\catcode13=12 \\def^^25^^25#1: #2^^M{\\gdef\\do{\\proc[#2]}}%@\\relax}{}\\def\\proc[#1 #2 #3 #4]{\\pdfhorigin=-#1bp\\relax\\pdfvorigin=#2bp\\relax\\pdfpagewidth=\\dimexpr#3bp-#1bp\\relax\\pdfpageheight=\\dimexpr#4bp-#2bp\\relax}\\do\\advance\\pdfhorigin by %ldbp\\relax\\advance\\pdfpagewidth by %ldbp\\relax\\advance\\pdfpagewidth by %ldbp\\relax\\advance\\pdfvorigin by -%ldbp\\relax\\advance\\pdfpageheight by %ldbp\\relax\\advance\\pdfpageheight by %ldbp\\relax\\setbox0=\\hbox{\\pdfximage page %ld mediabox{%@}\\pdfrefximage\\pdflastximage}\\ht0=\\pdfpageheight\\relax\\shipout\\box0\\relax", bbStr, leftmargin, leftmargin, rightmargin, bottommargin, bottommargin, topmargin, page, pdfPath];
@@ -492,10 +504,18 @@
     NSMutableString *cropTeX = [NSMutableString stringWithString:@"\\pdfoutput=1"];
 
     if (page > 0) {
-        [cropTeX appendString:[self buildCropTeXSource:pdfPath page:page addMargin:addMargin]];
+        NSString *cropTeXsource = [self buildCropTeXSource:pdfPath page:page addMargin:addMargin];
+        if (!cropTeXsource) {
+            return NO;
+        }
+        [cropTeX appendString:cropTeXsource];
     } else {
         for (NSUInteger i=1; i<=totalPages; i++) {
-            [cropTeX appendString:[self buildCropTeXSource:pdfPath page:i addMargin:addMargin]];
+            NSString *cropTeXsource = [self buildCropTeXSource:pdfPath page:i addMargin:addMargin];
+            if (!cropTeXsource) {
+                return NO;
+            }
+            [cropTeX appendString:cropTeXsource];
         }
     }
     [cropTeX appendString:@"\\end"];
