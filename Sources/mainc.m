1de62af700ed714ba6bc52764b7354d366da19c6
doraTeX <taylorkgb@gmail.com>
2016-12-15 15:58:53 +0900

    Ver. 2.1.7

--- Sources/mainc.m
+++ Sources/mainc.m
@@ -11,7 +11,7 @@
 #import "NSColor-Extension.h"
 
 #define OPTION_NUM 53
-#define VERSION "2.1.6"
+#define VERSION "2.1.7"
 #define DEFAULT_MAXIMAL_NUMBER_OF_COMPILATION 3
 
 #define ENABLED "enabled"
