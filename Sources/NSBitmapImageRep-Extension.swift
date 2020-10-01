a76c9a8b4bb9df4d7f4ee9a5573ab47dca148e13
doraTeX <taylorkgb@gmail.com>
2020-10-02 02:36:55 +0900

    微修正

--- Sources/NSBitmapImageRep-Extension.swift
+++ Sources/NSBitmapImageRep-Extension.swift
@@ -2,15 +2,14 @@ import Cocoa
 
 extension NSBitmapImageRep {
     @objc func representation(usingType type: CFString, usingDPI dpi: Int) -> Data? {
-        let prop : [String:NSNumber] = [
+        let prop = [
             kCGImageDestinationLossyCompressionQuality as String: 1.0,
             kCGImagePropertyDPIWidth as String: NSNumber(value: dpi),
             kCGImagePropertyDPIHeight as String: NSNumber(value: dpi)
         ]
 
         let data = NSMutableData() as CFMutableData
-        guard
-              let destination = CGImageDestinationCreateWithData(data, type, 1, nil),
+        guard let destination = CGImageDestinationCreateWithData(data, type, 1, nil),
               let cgImage = self.cgImage else { return nil }
 
         CGImageDestinationAddImage(destination, cgImage, prop as CFDictionary)
